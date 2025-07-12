5666bcd844234e1ebe6da0a2514cbdd47fd3d05d
ADIT PANDEY <153520131+aditpandey147@users.noreply.github.com>
2025-07-12 12:32:06 +0530

    push

--- fetch_cookies.sh
+++ fetch_cookies.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# Download cookies.txt from Google Drive or Dropbox
 echo "🔁 Downloading cookies.txt..."
-curl -L -o cookies.txt "https://drive.google.com/file/d/1Y-Vst2pQZGEA_SUWbFVDkUn0N_ZGltxF/view?usp=drive_link"
 
-echo "✅ cookies.txt downloaded!"
+curl -L -o cookies.txt "https://drive.google.com/file/d/1Y-Vst2pQZGEA_SUWbFVDkUn0N_ZGltxF/view?usp=sharing"
+
+if [ -f cookies.txt ]; then
+    echo "✅ cookies.txt downloaded!"
+else
+    echo "❌ Failed to download cookies.txt"
+fi
