3c0a24299ce1a3aa9bca0dd15352e823efcc2d1b
ADIT PANDEY <153520131+aditpandey147@users.noreply.github.com>
2025-07-13 11:47:58 +0530

    sasas

--- downloader/views.py
+++ downloader/views.py
@@ -4,6 +4,14 @@ from django.conf import settings
 from django.shortcuts import render
 from django.http import FileResponse, HttpResponse
 import random
+import urllib.request
+
+def download_cookies():
+    cookie_url = 'https://drive.google.com/uc?export=download&id=YOUR_FILE_ID'
+    cookie_path = os.path.join(settings.BASE_DIR, 'cookies.txt')
+    if not os.path.exists(cookie_path):
+        urllib.request.urlretrieve(cookie_url, cookie_path)
+
 
 def index(request):
     return render(request, 'downloader/index.html')
