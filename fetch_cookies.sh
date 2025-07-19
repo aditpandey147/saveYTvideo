80bfed36a74e1a09fc2a15cbb0bc30ef66b8fb90
ADIT PANDEY <153520131+aditpandey147@users.noreply.github.com>
2025-07-19 10:15:14 +0530

    push

--- fetch_cookies.sh
+++ fetch_cookies.sh
@@ -1,7 +1,7 @@
 # fetch_cookies.py
 import urllib.request
 
-url = "https://drive.google.com/uc?export=download&id=1Y-Vst2pQZGEA_SUWbFVDkUn0N_ZGltxF"
+url = "https://drive.google.com/uc?export=download&id=1lv2qcVGMC7jLT3cX7LXPWB9Qne8RHAC4"
 output_path = "cookies.txt"
 
 try:
