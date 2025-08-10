1a5de774555c55ce979179433dffee8723f031aa
ADIT PANDEY <153520131+aditpandey147@users.noreply.github.com>
2025-08-10 08:37:31 +0530

    push

--- yt_downloader/urls.py
+++ yt_downloader/urls.py
@@ -1,7 +1,27 @@
 from django.urls import path
 from downloader import views
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import Sitemap
+from django.urls import path
+from django.shortcuts import reverse
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('download/', views.download_video, name='download_video'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ]
+
+
+class StaticViewSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        return ['home', 'download'] 
+    
+    def location(self, item):
+        return reverse(item)
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
