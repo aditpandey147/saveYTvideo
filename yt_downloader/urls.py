d3e37f415cae7246036ae30a06c1dba81380ef3f
ADIT PANDEY <153520131+aditpandey147@users.noreply.github.com>
2025-08-10 11:01:49 +0530

    push

--- yt_downloader/urls.py
+++ yt_downloader/urls.py
@@ -10,7 +10,7 @@ class StaticViewSitemap(Sitemap):
     priority = 0.8
 
     def items(self):
-        return ['index', 'download_video'] 
+        return ['index', 'download_video' 'blog'] 
     
     def location(self, item):
         return reverse(item)
