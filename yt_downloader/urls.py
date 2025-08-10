de12500530ab538edbc112591f763417694fa174
ADIT PANDEY <153520131+aditpandey147@users.noreply.github.com>
2025-08-10 10:25:46 +0530

    push

--- yt_downloader/urls.py
+++ yt_downloader/urls.py
@@ -22,6 +22,6 @@ sitemaps = {
 urlpatterns = [
     path('', views.index, name='index'),
     path('download/', views.download_video, name='download_video'),
-    path('blog/', views.blog),
+    path('blog/', views.blog, name='blog'),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='sitemap'),
 ]
