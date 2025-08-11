cf64be2edbcd6907daff086bd59d1b8bc1700319
ADIT PANDEY <153520131+aditpandey147@users.noreply.github.com>
2025-08-11 09:23:03 +0530

    PUSH

--- downloader/models.py
+++ downloader/models.py
@@ -1,17 +1,3 @@
 from django.db import models
-from django.utils.text import slugify
 
-class Blog(models.Model):
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True, blank=True)
-    image = models.ImageField(upload_to='blog_images/')
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(self.title)
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.title
+# Create your models here.
